aa1e286fe18361f5d8144bf760a174cb199051c6
unknown <mesgu2@pdx.edu>
2019-07-25 16:30:52 -0700

    tutorial stuff

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello from a module!")
+
+	const c = 3
+	fmt.Println(c + 3)
+
+	// a bunch of code
+
+	const pi = 3.1415
+	fmt.Println(pi)
+
+	fmt.Println(c + 1.2)
+
 }
